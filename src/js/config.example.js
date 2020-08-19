8a25cd0798e2913f2c556c4e655a7de3ec717671
Toni Mägel <tmaegel@posteo.de>
2020-08-19 15:38:05 +0200

    updated example config file

--- src/js/config.example.js
+++ src/js/config.example.js
@@ -2,8 +2,7 @@
 
 // Config
 export const DEBUG = false;
-export const REST_SERVER = 'https://localhost';
+export const REST_SERVER = 'http://localhost';
 export const REST_PORT = '3000';
 export const REST_PATH = '';
 export const DOMAIN = 'localhost';
-export const CHART_ID = 'chart';
